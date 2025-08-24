1cb272d03de9e2371344f01d42ca6e48b55b26ba
Jacek Laskowski <jacek@japila.pl>
2025-08-24 15:41:00 +0200

    Use podman (not docker) to build images

--- build-image.sh
+++ build-image.sh
@@ -15,15 +15,14 @@ books () { cd ~/books }
 
 # FIXME Command-line option to trigger it
 remove_all_images () {
-  docker rmi -f $(docker image ls 'squidfunk/mkdocs-material-insiders:*' -q)
-  docker rmi -f $(docker image ls 'jaceklaskowski/mkdocs-material-insiders:*' -q)
+  podman rmi -f $(podman image ls 'squidfunk/mkdocs-material-insiders:*' -q)
+  podman rmi -f $(podman image ls 'jaceklaskowski/mkdocs-material-insiders:*' -q)
 }
 
-# In the repo directory, execute `docker build` as follows:
-oss; cd mkdocs-material-insiders; git pull; gco $INSIDERS_TAG
+oss; cd mkdocs-material; git pull; gco $INSIDERS_TAG
 
 # FIXME Command-line option to enable --no-cache
-docker build \
+podman build \
   --no-cache \
   --tag squidfunk/mkdocs-material-insiders \
   --tag squidfunk/mkdocs-material-insiders:$INSIDERS_TAG \
@@ -33,7 +32,7 @@ gco master
 
 books; cd japila-books.github.io
 
-docker build \
+podman build \
   --no-cache \
   --build-arg INSIDERS_TAG=$INSIDERS_TAG \
   --tag jaceklaskowski/mkdocs-material-insiders \
@@ -42,10 +41,10 @@ docker build \
 
 # Clean Up
 
-docker rmi \
-  $(docker image ls 'squidfunk/mkdocs-material-insiders' \
+podman rmi \
+  $(podman image ls 'squidfunk/mkdocs-material-insiders' \
       --filter=before=squidfunk/mkdocs-material-insiders:$INSIDERS_TAG -q)
 
-docker rmi \
-  $(docker image ls 'jaceklaskowski/mkdocs-material-insiders' \
+podman rmi \
+  $(podman image ls 'jaceklaskowski/mkdocs-material-insiders' \
       --filter=before=jaceklaskowski/mkdocs-material-insiders:latest -q)
